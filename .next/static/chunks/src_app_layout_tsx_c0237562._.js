5cdc068832611e82f83a604d7c64c1477f8a82b0
saroel01 <142075053+saroel01@users.noreply.github.com>
2025-06-16 18:41:09 +0700

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1750074085499

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__8c5b3ecc._.css",
+  "static/chunks/node_modules_d7639ffa._.js",
+  "static/chunks/src_bb2bba30._.js"
+],
+    source: "dynamic"
+});
