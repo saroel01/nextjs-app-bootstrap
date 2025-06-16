0353ca30ca2b2f38bb455ee59bbfa2683491ca8e
saroel01 <142075053+saroel01@users.noreply.github.com>
2025-06-16 18:40:49 +0700

    Add .next/static/chunks/src_app_page_tsx_03c8dc82._.js 1750074065401

---
+++ .next/static/chunks/src_app_page_tsx_03c8dc82._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_0b34a674._.js"
+],
+    source: "dynamic"
+});
